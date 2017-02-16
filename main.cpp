2b56b1d8a1de0302b8b6ccb49cd915e75867db75
neiljohnson <mr.neil.j@gmail.com>
2017-02-15 16:32:25 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// This program uses two range-based for loops with a vector
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // Define a vector with a starting size of 5 elements
+    vector<int> number(5);
+
+    // Get values for the vector elemetns
+    // Make the range variable a reference variable so it can be
+    // uset to change the contents of the lement it references
+    
+}
